be58cd7cc23d158d670f9ed91a3d4e81fe852780
Wojciech Hubczak (ext) <wojciech.hubczak.external@atos.net>
2023-08-01 23:49:33 +0200

    Adding 'CHECKOUT: OVERVIEW' page tests

---
+++ cypress/e2e/checkoutOverview.cy.js
@@ -0,0 +1,174 @@
+/// <reference types ="Cypress"/>
+
+import { myClassTestDescAndDate } from "./myClassTestDescAndDate";
+
+let loginy = Cypress.env("swagTesting");
+let checkoutDetails = Cypress.env("checkoutInfo");
+
+describe("\n5. 'CHECKOUT: OVERVIEW' PAGE TEST\n", () => {
+    it(`Test 1/1 - \'CHECKOUT: OVERVIEW\' appearance test.\n${myClassTestDescAndDate.testTimeAndDate()}`, () => {
+        cy.createSession(loginy.username, loginy.password);
+        cy.visit("/inventory.html");
+
+        cy.get(".shopping_cart_link").should("be.visible").click(); //przejdz do koszyka
+        cy.url().should("contain", "/cart.html");
+        cy.get("#checkout").should("be.visible").click(); //przejdz do checkout-step-one.html
+
+        cy.fillInCheckoutForm(
+            checkoutDetails.firstName,
+            checkoutDetails.lastName,
+            checkoutDetails.postCode
+        );
+
+        cy.url().should("contain", "/checkout-step-two.html");
+        cy.get(".shopping_cart_link").should("be.empty");
+        cy.get(".cart_list").children().should("have.length", 2); //brak productow ('.cart_item') w koszyku
+        cy.get(".cart_quantity_label").should("have.text", "QTY");
+        cy.get(".cart_desc_label").should("have.text", "DESCRIPTION");
+
+        cy.get(".summary_info")
+            .contains("Payment Information:")
+            .should("have.text", "Payment Information:");
+        cy.get(".summary_info")
+            .contains("SauceCard #31337")
+            .should("have.text", "SauceCard #31337");
+        cy.get(".summary_info")
+            .contains("Shipping Information:")
+            .should("have.text", "Shipping Information:");
+        cy.get(".summary_info")
+            .contains("FREE PONY EXPRESS DELIVERY!")
+            .should("have.text", "FREE PONY EXPRESS DELIVERY!");
+
+        cy.checkHeader("Checkout: Overview");
+        cy.checkFooter();
+
+        cy.get("#finish").should("be.visible");
+        cy.get("#cancel").should("be.visible").click();
+        cy.url().should("contain", "/cart.html");
+        //cy.url().should("contain", "/inventory.html");
+
+        //DEFEKT - po podmianie "/cart.html" na  "/inventory.html" przechodzi test
+    });
+
+    it(`Test 1/1 - \'CHECKOUT: OVERVIEW\' with full cart test.\n${myClassTestDescAndDate.testTimeAndDate()}`, () => {
+        cy.createSession(loginy.username, loginy.password);
+        cy.visit("/inventory.html");
+        cy.addEachProductToCart(); //dodaj wszystkie
+        cy.get(".shopping_cart_link").should("be.visible").click(); //przejdz do koszyka
+
+        cy.url().should("contain", "/cart.html");
+        cy.get("#checkout").should("be.visible").click(); //przejdz do checkout-step-one.html
+        //zrob checkout i przejdz do checkout:Overview
+        cy.fillInCheckoutForm(
+            checkoutDetails.firstName,
+            checkoutDetails.lastName,
+            checkoutDetails.postCode
+        );
+
+        cy.url().should("contain", "/checkout-step-two.html");
+        cy.get(".shopping_cart_link")
+            .should("not.be.empty")
+            .children()
+            .should("have.text", 6);
+        cy.get(".cart_list").children().should("have.length", 8); //2+6 czyli 6 productow ('.cart_item') w koszyku\\
+
+        let counter = 0;
+        cy.fixture("storeArticles").then((products) => {
+            products.forEach((product) => {
+                cy.checkCartProductDesc(product); //testowanie opisow, ilosci i cen w koszyku
+                //sprawdz czy suma cen produktow = cenie wyswietlanej w 'Item total:'
+                cy.get(".cart_item")
+                    .find(".inventory_item_price")
+                    .should("have.length", 6 - counter)
+                    .map("innerText") //pobierz tablice cen
+                    .mapInvoke("slice", 1)
+                    .map(Number)
+                    .apply(Cypress._.sum)
+                    .then((p) => Cypress._.round(p, 2)) //zaokraglij do 2 po przecinku
+
+                    //DEFEKT -> po zakomentowaniu powyzszej instrukcji test przechodzi
+
+                    .then((z) => {
+                        let sum1 = z;
+                        cy.get(".summary_info") //
+                            .contains("Item total: $")
+                            .map("innerText")
+                            .mapInvoke("slice", 13)
+                            .map(Number)
+                            .apply(Cypress._.sum)
+                            .then((y) => {
+                                let sum2 = y;
+                                expect(sum1).to.be.deep.equal(sum2);
+                            });
+                        //sprawdz czy tax obliczony z sumy cen = tax wyswietlany w 'Tax:'
+                        let tax1 = Cypress._.round((sum1 * 8) / 100, 2); //policz tax 8% i zaokraglij do 2
+                        cy.get(".summary_info")
+                            .contains("Tax: $")
+                            .map("innerText")
+                            .mapInvoke("slice", 6)
+                            .map(Number)
+                            .apply(Cypress._.sum)
+                            .then((n) => {
+                                let tax2 = n;
+                                expect(tax1).to.be.deep.equal(tax2);
+                            });
+                        //sprawdz czy suma + tax obliczone z cen = total wyswietlany w 'Total:'
+                        let total1 = Cypress._.round(sum1 + tax1, 2); //policz suma + tax i zaokraglij do 2
+                        cy.get(".summary_info")
+                            .contains("Total: $")
+                            .map("innerText")
+                            .mapInvoke("slice", 8)
+                            .map(Number)
+                            .apply(Cypress._.sum)
+                            .then((m) => {
+                                let total2 = m;
+                                expect(total1).to.be.deep.equal(total2);
+                            });
+                    });
+
+                cy.get(".shopping_cart_link").should("be.visible").click(); //przejdz do '/cart.html'
+                cy.get(".cart_list")
+                    .contains("div", product.article)
+                    .parent()
+                    .siblings()
+                    .eq(1)
+                    .children()
+                    .eq(1)
+                    .should("contain", "Remove")
+                    .click(); //usun z koszyka
+
+                cy.get("#checkout").should("be.visible").click(); //przejdz do checkout-step-one.html
+                cy.fillInCheckoutForm(
+                    //zrob checkout i przejdz do checkout:Overview
+                    checkoutDetails.firstName,
+                    checkoutDetails.lastName,
+                    checkoutDetails.postCode
+                );
+
+                counter = counter + 1;
+                cy.get(".cart_list")
+                    .children()
+                    .should("have.length", 8 - counter);
+
+                if (counter == 6) {
+                    cy.get(".shopping_cart_link").should("be.empty");
+                } else {
+                    cy.get(".shopping_cart_link")
+                        .should("not.be.empty")
+                        .children()
+                        .should("have.text", 6 - counter);
+                }
+            });
+        });
+        //sprawdz podsumowanie cen po usunieciu zawartosci koszyka
+        cy.get(".summary_info")
+            .contains("Item total: $")
+            .should("have.text", "Item total: $0");
+        cy.get(".summary_info")
+            .contains("Tax: $")
+            .should("have.text", "Tax: $0.00");
+        cy.get(".summary_info")
+            .contains("Total: $")
+            .should("have.text", "Total: $0.00");
+    });
+});
---
+++ cypress/e2e/checkoutOverviewMenu.cy.js
@@ -0,0 +1,124 @@
+/// <reference types ="Cypress"/>
+
+import burgerMenuItems from "../fixtures/burgerMenu.json";
+import { myClassTestDescAndDate } from "./myClassTestDescAndDate";
+
+let loginy = Cypress.env("swagTesting");
+let checkoutDetails = Cypress.env("checkoutInfo");
+
+describe(
+    "\n5.1 'CHECKOUT: OVERVIEW' PAGE - BURGER MENU TEST\n",
+    { testIsolation: false },
+    () => {
+        it("Login - Reset App State - Logout", () => {
+            cy.myLoginSwagStore(loginy.username, loginy.password); //loguj
+            cy.get("#react-burger-menu-btn").should("be.visible").click();
+            cy.get(".bm-item-list").contains("Reset App State").click();
+            cy.get(".bm-item-list").contains("Logout").click();
+        });
+
+        it.each(burgerMenuItems)(
+            (z, l, t) =>
+                myClassTestDescAndDate.testDescription(
+                    z,
+                    l,
+                    t,
+                    "testingBurgerMenu"
+                ),
+            (menu) => {
+                if (menu.menuItem == "All Items") {
+                    cy.myLoginSwagStore(loginy.username, loginy.password); //loguj
+                }
+                if (menu.menuItem == "Reset App State") {
+                    // po poprzednim kroku 'Logout'
+                    cy.myLoginSwagStore(loginy.username, loginy.password); //loguj
+                    cy.checkIfAddToCartButtonReset("Remove", 6); //czy pamieta stan koszyka i buttonow po poprzednim kroku 'Logout'
+                }
+                if (menu.menuItem == "Logout") {
+                    cy.visit("/inventory.html");
+                } //tylko opja 'Logout' tego wymaga
+
+                cy.get(".shopping_cart_link").should("be.visible").click(); //przejdz do koszyka
+                cy.url().should("contain", "/cart.html");
+                cy.get("#checkout").should("be.visible").click(); //przejdz do checkout-step-one.html
+                cy.fillInCheckoutForm(
+                    //zrob checkout i przejdz do checkout:Overview
+                    checkoutDetails.firstName,
+                    checkoutDetails.lastName,
+                    checkoutDetails.postCode
+                );
+                cy.url().should("contain", "/checkout-step-two.html");
+                cy.get("#react-burger-menu-btn").should("be.visible").click(); //burger menu button widoczny
+                cy.get(".bm-item-list").should("be.visible"); //menu musi byc widoczne
+                cy.get(".bm-item-list")
+                    .contains(menu.menuItem)
+                    .should("have.text", menu.menuItem); //czy zgodne z opisem
+
+                if (menu.menuItem != "About") {
+                    //z wyjatkiem 'About' bo 'https://saucelabs.com/' sie dlugo laduje i wywala test
+
+                    if (menu.menuItem == "Reset App State") {
+                        //czy pamieta stan koszyka w '/checkout-step-two.html' po ponownym zalogowaniu (poprzedni krok 'Logout')
+                        cy.get(".shopping_cart_link")
+                            .should("not.be.empty")
+                            .children()
+                            .should("have.text", 6); //cyfra z iloscia w ikonie koszyka
+                        cy.get(".cart_list")
+                            .children()
+                            .should("have.length", 8); //ilosc w koszyku
+                        cy.fixture("storeArticles").then((products) => {
+                            //opisy produktow
+                            products.forEach((product) => {
+                                cy.checkCartProductDesc(product);
+                            });
+                        });
+
+                        cy.get(".bm-item-list")
+                            .contains("Reset App State")
+                            .click(); //resetuje Apke
+                        //cy.checkPageReload('Reset App State') // tak zamiast powyzszej ale wiem, ze nie odswieza
+                        //cy.get('#react-burger-menu-btn').should('be.visible').click() //burger menu button widoczny
+                        cy.get(".bm-cross-button").should("be.visible").click();
+                        cy.get(".bm-item-list").should("not.be.visible"); //sprawdzenie czy dziala button 'X' zamykajacy menu
+
+                        cy.get(".shopping_cart_link").should("be.empty"); //spawdza koszyk w '/checkout-step-two.html'
+
+                        //DEFEKT - nie odswieza strony po 'Reset App State'.
+                        //cy.reload(); //wymusza odswiezenie strony
+                        //Po odkomentowaniu powyzszej instrukcji przechodzi test
+
+                        cy.get(".cart_list")
+                            .children()
+                            .should("have.length", 2); //spawdza '.cart_list' czy elemety usuniete koszyka
+
+                        cy.get("#cancel").should("be.visible").click(); //powrot do inventory
+
+                        //DEFEKT - powyzsze powinno przekierowac do /cart.html i dopiero pozniej
+                        //cy.get("#continue-shopping").should("be.visible").click(); //powrot do inventory
+
+                        cy.checkIfAddToCartButtonReset("Add to cart", 0); //czy buttony 'Add to cart' w zresetowane w '/inventory.html'
+                    } else {
+                        cy.get(".bm-item-list").contains(menu.menuItem).click(); //klika w aktualna opcje zeby sprawdzic przekierowanie
+                        cy.url().should("contain", menu.action); //czy dziala przekierowanie ?
+                        if (menu.menuItem == "All Items") {
+                            cy.addEachProductToCart(); //w pierwszym kroku dodaj wszystkie produkty do koszyka
+                        }
+                    }
+                } else {
+                    //jesli opcja: 'About'
+                    cy.get(".shopping_cart_link")
+                        .should("not.be.empty")
+                        .children()
+                        .should("have.text", 6); //spawdza koszyk w '/checkout-step-one.html'
+                    cy.get(".bm-item-list")
+                        .contains(menu.menuItem)
+                        .should("have.attr", "href"); //sprawdza link dla opcji 'About'
+                    cy.get(".bm-item-list")
+                        .contains(menu.menuItem)
+                        .invoke("attr", "href")
+                        .should("contain", "https://saucelabs.com/");
+                }
+            }
+        );
+    }
+);
