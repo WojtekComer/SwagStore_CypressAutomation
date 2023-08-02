3c58f61a67832a1ec9feded48e0bbbfef42b0e29
Wojciech Hubczak (ext) <wojciech.hubczak.external@atos.net>
2023-08-02 17:27:21 +0200

    Adding new tests for 'CHECKOUT: COMPLETE' page

---
+++ cypress/e2e/checkoutComplete.cy.js
@@ -0,0 +1,61 @@
+/// <reference types ="Cypress"/>
+
+import { myClassTestDescAndDate } from "./myClassTestDescAndDate";
+
+let loginy = Cypress.env("swagTesting");
+let checkoutDetails = Cypress.env("checkoutInfo");
+
+describe("\n6. 'CHECKOUT: COMPLETE' PAGE TEST\n", () => {
+    it(`Test 1/1 - \'CHECKOUT: COMPLETE\' page test.\n${myClassTestDescAndDate.testTimeAndDate()}`, () => {
+        cy.myLoginSwagStore(loginy.username, loginy.password); //loguj
+        cy.visit("/inventory.html");
+        cy.addEachProductToCart(); //dodaj wszystkie
+        cy.get(".shopping_cart_link").should("be.visible").click(); //przejdz do koszyka
+        cy.url().should("contain", "/cart.html");
+        cy.get("#checkout").should("be.visible").click(); //przejdz do checkout-step-one.html
+        cy.fillInCheckoutForm(
+            //zrob checkout i przejdz do checkout:Overview
+            checkoutDetails.firstName,
+            checkoutDetails.lastName,
+            checkoutDetails.postCode
+        );
+        cy.get("#finish").should("be.visible").click();
+        cy.url().should("contain", "/checkout-complete.html"); //przejdz do 'checkout: Complete!'
+
+        cy.get(".shopping_cart_link").should("be.visible"); //czy koszyk widoczny i pusty
+        cy.get(".shopping_cart_link").should("be.empty").click(); //przejdz do koszyka w '/cart.html'
+        cy.url().should("contain", "/cart.html");
+        cy.get(".shopping_cart_link").should("be.empty"); //sprawdz koszyk w '/cart.html'
+        cy.get(".cart_list").children().should("have.length", 2); //spawdz '.cart_list' czy elemety usuniete z koszyka
+        cy.get("#continue-shopping").should("be.visible").click(); //przejdz do '/inventory.html'
+        cy.url().should("contain", "/inventory.html");
+        cy.get(".shopping_cart_link").should("be.empty"); //sprawdz koszyk w '/inventory.html'
+        cy.checkIfAddToCartButtonReset("Add to cart", 0); //sprawdz czy buttony zresetowane w '/inventory.html'
+        cy.visit("/checkout-complete.html"); //przejdz do 'checkout: Complete!'
+
+        cy.checkHeader("Checkout: Complete!");
+        cy.checkFooter();
+
+        cy.get("#checkout_complete_container")
+            .children()
+            .eq(0)
+            .should("have.text", "THANK YOU FOR YOUR ORDER");
+        cy.get("#checkout_complete_container")
+            .children()
+            .eq(1)
+            .should(
+                "have.text",
+                "Your order has been dispatched, and will arrive just as fast as the pony can get there!"
+            );
+        cy.get("#checkout_complete_container")
+            .children()
+            .eq(2)
+            .should("be.visible");
+        cy.get("#checkout_complete_container")
+            .children()
+            .eq(3)
+            .should("have.text", "Back Home")
+            .click();
+        cy.url().should("contain", "/inventory.html");
+    });
+});
---
+++ cypress/e2e/checkoutCompleteMenu.cy.js
@@ -0,0 +1,83 @@
+/// <reference types ="Cypress"/>
+
+import burgerMenuItems from "../fixtures/burgerMenu.json";
+import { myClassTestDescAndDate } from "./myClassTestDescAndDate";
+
+let loginy = Cypress.env("swagTesting");
+let checkoutDetails = Cypress.env("checkoutInfo");
+
+describe(
+    "\n6.1 'CHECKOUT: COMPLETE' PAGE - BURGER MENU TEST\n",
+    { testIsolation: false },
+    () => {
+        before("Login - Reset App State - Logout", () => {
+            cy.resetAppState(loginy.username, loginy.password);
+        });
+
+        it.each(burgerMenuItems)(
+            (z, l, t) =>
+                myClassTestDescAndDate.testDescription(
+                    z,
+                    l,
+                    t,
+                    "testingBurgerMenu"
+                ),
+            (menu) => {
+                if (menu.menuItem == "All Items") {
+                    cy.myLoginSwagStore(loginy.username, loginy.password); //loguj
+                }
+                if (menu.menuItem == "Reset App State") {
+                    // po poprzednim kroku 'Logout'
+                    cy.myLoginSwagStore(loginy.username, loginy.password); //loguj
+                    cy.checkIfAddToCartButtonReset("Add to cart", 0); //!!! tu w zaleznosci od parametrow 0 lub 6!
+                    //albo albo tylko sprawdzic cyfre w koszyku na /inventory.html'
+                }
+                if (menu.menuItem == "Logout") {
+                    cy.visit("/inventory.html");
+                } //tylko opja 'Logout' tego wymaga
+
+                cy.get(".shopping_cart_link").should("be.visible").click(); //przejdz do koszyka
+                cy.url().should("contain", "/cart.html");
+                cy.get("#checkout").should("be.visible").click(); //przejdz do checkout-step-one.html
+                cy.fillInCheckoutForm(
+                    //zrob checkout i przejdz do checkout:Overview
+                    checkoutDetails.firstName,
+                    checkoutDetails.lastName,
+                    checkoutDetails.postCode
+                );
+                cy.url().should("contain", "/checkout-step-two.html"); //zrobic tu funcjke checkCurrentMenuOption(option)
+                cy.get("#finish").should("be.visible").click();
+                cy.url().should("contain", "/checkout-complete.html"); //przejdz do 'checkout: Complete!'
+                cy.get("#react-burger-menu-btn").should("be.visible").click(); //burger menu button widoczny
+                cy.get(".bm-item-list").should("be.visible"); //menu musi byc widoczne
+                cy.get(".bm-item-list")
+                    .contains(menu.menuItem)
+                    .should("have.text", menu.menuItem); //czy zgodne z opisem
+
+                if (menu.menuItem != "About") {
+                    //z wyjatkiem 'About' bo 'https://saucelabs.com/' sie dlugo laduje i wywala test
+
+                    if (menu.menuItem == "Reset App State") {
+                        cy.get(".shopping_cart_link").should("be.empty");
+                        cy.get(".bm-item-list")
+                            .contains("Reset App State")
+                            .click(); //resetuje Apke
+                        //cy.checkPageReload('Reset App State') // tak zamiast powyzszej ale wiem, ze nie odswieza
+                        //cy.get('#react-burger-menu-btn').should('be.visible').click() //burger menu button widoczny
+                        cy.get(".bm-cross-button").should("be.visible").click();
+                        cy.get(".bm-item-list").should("not.be.visible"); //sprawdzenie czy dziala button 'X' zamykajacy menu
+
+                        //DEFEKT - nie odswieza strony po 'Reset App State'.
+                        //cy.reload(); //wymusza odswiezenie strony
+                        //Po odkomentowaniu powyzszej instrukcji przechodzi test
+                    } else {
+                        cy.get(".bm-item-list").contains(menu.menuItem).click(); //klika w aktualna opcje zeby sprawdzic przekierowanie
+                        cy.url().should("contain", menu.action); //czy dziala przekierowanie ?
+                    }
+                } else {
+                    cy.ifMenuOptionAbout("Complete");
+                }
+            }
+        );
+    }
+);
